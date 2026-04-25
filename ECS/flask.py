23ecc7cdb64575620bda769ef32be31fde2364a2
SENGLONGTE <s4054917@student.rmit.edu.au>
2026-04-25 10:14:01 +0700

    Implemnted login function on EC2 Flask

--- ECS/flask.py
+++ ECS/flask.py
@@ -13,7 +13,8 @@ sys.path.append(str(project_root))
 from utils.DynamoDB import DynamoDB
 from utils.S3 import S3
 
-# Initialise DBs and S3
+# Initialise DBs and S3 -----------------------------------------------------------------------------------
+
 songsDB = DynamoDB()
 songsDB.get_table("music")
 
@@ -23,6 +24,35 @@ loginDB.get_table("login")
 s3 = S3()
 s3.get_bucket('cloud-computing-a2-s4054917')
 
+# AUTH -----------------------------------------------------------------------------------------------------
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    result = loginDB.query(keyString='email', query=email)[0]
+
+    print(f"result: {result}")
+
+    if result is None:
+        return jsonify({'message': 'User not found'}), 404
+    if password == result['password']:
+
+        session['email'] = email
+        session['username'] = result['user_name']
+
+        return jsonify({
+            'message': 'Login successful',
+            'email': email,
+            'username': result['user_name']
+        }), 200
+    
+    else:
+        return jsonify({'message': 'Incorrect password'}), 200
 
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
